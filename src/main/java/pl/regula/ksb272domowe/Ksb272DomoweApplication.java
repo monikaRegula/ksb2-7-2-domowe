57722e7b5f0c51c81e7c30b9d61b4bd3d01d2cf3
monikaRegula <monikaregula97@gmail.com>
2023-05-25 11:28:37 +0200

    Initial commit

---
+++ src/main/java/pl/regula/ksb272domowe/Ksb272DomoweApplication.java
@@ -0,0 +1,13 @@
+package pl.regula.ksb272domowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksb272DomoweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ksb272DomoweApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/regula/ksb272domowe/Ksb272DomoweApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.regula.ksb272domowe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ksb272DomoweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
